9251561fbd4a193caf36a5b6484f2767393c3d51
TheLostone145 <martinthegamer145@gmail.com>
2024-12-06 12:12:54 -0500

    Meh

--- app/src/main/java/searchsort/Search.java
+++ app/src/main/java/searchsort/Search.java
@@ -8,19 +8,13 @@ public abstract class Search {
      * Find the index of the target element in the sorted array arr using binary
      * search. Return -1 if target is not found in arr.
      */
-    abstract public int find(int[] arr, int target){
+    abstract public int find(int[] arr, int target);
 
     /**
      * Recursively find the index of the target element in the sorted array arr
      * using binary. Return -1 if target is not found in arr.
      */
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]==target){
-                return i;
-            }
-        }
-        return -1;
-    }
+    
     abstract public int recursiveFind(int[] arr, int target);
 
     /**
